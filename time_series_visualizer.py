14d2e2cf1a44a5cdbcde3f889fd2023ceb08773a
girumdms <164764083+girumdms@users.noreply.github.com>
2024-04-05 18:33:06 +0200

    initial

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -13,6 +13,12 @@ df = df.value[(df.value > df.value.quantile(0.025)) & (df.value < df.value.quant
 
 def draw_line_plot():
     # Draw line plot
+    plt.figure(figsize=(30,8))
+    plt.plot(df['date'], df['value'], color='r')
+    plt.title('Daily freecodecamp Forum Page Views 5/2016-12/2019')
+    plt.xlabel('Date')
+    plt.ylabel('Page Views')
+    plt.show()
 
 
 
@@ -24,10 +30,13 @@ def draw_line_plot():
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = None
-
+    df_bar = df.groupby([df.index.year, df.index.month_name()]).mean().value.unstack()
+    g= df.groupby([df.index.month_name(), df.index.year]).mean().value.unstack()
+    #df_bar1 = df_bar[list(calendar.month_name)[1:]]
+    
     # Draw bar plot
-
+    df_bar.columns.name = "Months"
+    fig = df_bar.plot(kind= 'bar', figsize= (12,6), xlabel='Years', ylabel = 'Average page views')
 
 
 
@@ -44,6 +53,8 @@ def draw_box_plot():
     df_box['month'] = [d.strftime('%b') for d in df_box.date]
 
     # Draw box plots (using Seaborn)
+    sns.boxplot(data=g, ax = ax[0]).set(xlabel = 'Year', ylabel = 'Page Views', title = 'Year-wise Box Plot (Trend)')
+    sns.boxplot(data=df_bar1, ax = ax[1]).set(xlabel = 'Month', ylabel = 'Page Views',title = 'Month-Wise Box Plot (Seasonality)')
 
 
 
